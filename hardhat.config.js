ed82da26426d4d51f6ab3feeb9eaa0f795474fa4
Gerardo Ornelas <gerardo@ornelastechnologies.com>
2023-02-08 19:47:03 -0600

    add deployment to  goerli

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
+require("@nomiclabs/hardhat-ethers")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
-};
+  defaultNetwork: "goerli",
+  networks: {
+    hardhat: {},
+    goerli: {
+      url: `${process.env.ALCHEMY_API_URL}`,
+      accounts: [`0x${process.env.METAMASK_PRIVATE_KEY}`],
+    },
+  },
+}
